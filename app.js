1166293d87fff5a32c3e0cdf9df533c0e5fb04ac
shahzod <shoxzodbek15@gmail.com>
2024-09-24 11:29:53 +0500

    open project

---
+++ app.js
@@ -0,0 +1,61 @@
+// If 1
+// let son = prompt('Butun son kiriting:');
+// son = parseInt(son);
+// if (son > 0){
+//     son++;
+// }
+// console.log("Hosil bo'lgan son: "  + son);
+
+
+// If 2
+// let son = prompt('Butun son kiriting: ');
+// son = parseInt(son);
+// if (son > 0) {
+//     console.log(son + 1);
+// }  else if (son < 0) {
+//     console.log(son - 2);
+// }else {
+//     console.log(son + 10)
+// }
+
+// If 3
+// let son1 = prompt('1chi sonni kiriting: ');
+// let son2 = prompt('2chi sonni  kiriting: ');
+// let son3 = prompt('3ch  sonni kiriting: ');
+
+// let bosh = 0;
+// son1 = parseInt(son1);
+
+// son2 = parseInt(son2);
+// son3 = parseInt(son3);
+
+
+// if (son1 > 0) bosh++;
+// if (son2 > 0) bosh++;
+// if (son3 > 0) bosh++;
+// console.log("Musbat sonlar:" + bosh);
+
+// If 4
+// let son1 = prompt('1-sonni kiriting');
+// let son2 = prompt('2-sonni kiriting');
+
+// son1 = parseInt(son1);
+// son2 = parseInt(son2);
+
+// if ( son1 > son2){
+//     console.log("Katta son: " + son1);
+// }else if(son1 < son2){
+//     console.log("Katta son: " + son2);
+// }else{
+//     console.log("Ikkala son teng")
+// }
+
+
+// If 5
+let son1 = prompt('1-sonni kiriting');
+let son2 = prompt('2-sonni kiriting');
+
+son1 = parseInt(son1);
+son2 = parseInt(son2);
+
+if 
