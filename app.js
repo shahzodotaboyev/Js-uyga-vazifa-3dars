463338a8627c92236c978798e7f186f0bd3b1284
shahzod <shoxzodbek15@gmail.com>
2024-09-24 12:23:29 +0500

    open project

--- app.js
+++ app.js
@@ -52,10 +52,110 @@
 
 
 // If 5
-let son1 = prompt('1-sonni kiriting');
-let son2 = prompt('2-sonni kiriting');
+// let son1 = prompt('1-sonni kiriting');
+// let son2 = prompt('2-sonni kiriting');
 
-son1 = parseInt(son1);
-son2 = parseInt(son2);
+// son1 = parseInt(son1);
+// son2 = parseInt(son2);
 
-if 
+// if (son1 < son2){
+//     console.log("Kichkina son:  " + son1);
+// }else if(son1 > son2){
+//     console.log("kichkina son: " + son2);
+// }else{
+//     console.log("Ikkala son teng");
+// }
+
+
+// If 6
+
+// let son1 = prompt('1-sonni kiriting');
+// let son2 = prompt('2-sonni kiriting');
+
+// son1 = parseInt(son1);
+// son2 = parseInt(son2);
+
+
+// if (son1 > son2) {
+//     console.log(son1);
+//     console.log(son2);
+
+// }else if (son1 < son2) {
+//     console.log(son2);
+//     console.log(son1);
+// }else{
+//     console.log("Ikkasiham teng")
+//     console.log(son1 )
+//     console.log(son2)
+// }
+
+// If 7
+// let a = prompt('a sonni kiriting');
+// let b = prompt('b sonni kiriting');
+
+// a = parseInt(a);
+// b = parseInt(b);
+
+// if (a > b) {
+//     let c = a;
+
+//     a = b;
+//     b = c;
+//     console.log("a ning qiymati: " + a);
+//     console.log("b ning qiymat: " + b);
+// } else if (a < b) {
+//     console.log("a ning qiymati: " + a);
+//     console.log("b ning qiymati: " + b);
+// }else{
+//     console.log("Ikkalasiniha qiymati bir")
+// }
+
+
+
+// If 8
+// let a = prompt('a sonni kiriting');
+// let b = prompt('b sonni kiriting');
+
+// a = parseInt(a);
+// b = parseInt(b);
+
+// if (a != b) {
+//     console.log("a ning qimati: " + (a + b));
+//     console.log("b ning qimati: " + (a + b));
+// } else if (a = b) {
+//     a = 0;
+//     b = 0;
+//     console.log("a ning qimati: " + a);
+//     console.log("b ning qimati: " + b);
+
+// }
+
+
+
+
+
+
+
+
+
+
+// If 16
+// function funksiya() {
+//     let x = parseFloat(prompt("X qiymatini kiriting:"));
+
+//     if (x <= 0) {
+//         return -x;
+//     }
+//     if (x > 0 && x < 2) {
+//         return x * x;
+//     }
+//     if (x >= 2) {
+//         return 4;
+//     }
+// }
+
+// console.log("Natija: " + funksiya());
+
+
+
+// If 17
